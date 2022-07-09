b819d3aca8f17b5a41e0d4638ee34d941729dd12
yoavti <yoavti@post.bgu.ac.il>
2022-07-09 20:18:28 +0300

    Added classifier_switcher.py

---
+++ model_selection/classifier_switcher.py
@@ -0,0 +1,22 @@
+from sklearnex import patch_sklearn
+patch_sklearn()
+from sklearn.base import BaseEstimator
+from sklearn.linear_model import SGDClassifier
+
+
+class ClfSwitcher(BaseEstimator):
+    def __init__(self, estimator=SGDClassifier()):
+        self.estimator = estimator
+
+    def fit(self, X, y=None, **kwargs):
+        self.estimator.fit(X, y)
+        return self
+
+    def predict(self, X, y=None):
+        return self.estimator.predict(X)
+
+    def predict_proba(self, X):
+        return self.estimator.predict_proba(X)
+
+    def score(self, X, y):
+        return self.estimator.score(X, y)
