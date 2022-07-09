7bfcbd135a7c59264931db19237c512932c709b7
yoavti <yoavti@post.bgu.ac.il>
2022-07-09 13:26:50 +0300

    Increased alpha

--- toy/toy.py
+++ toy/toy.py
@@ -21,4 +21,4 @@ def load_toy(path=FILENAME):
 if __name__ == '__main__':
     X, y = load_toy()
     alpha = lfs(X, y, gamma=-1)
-    w = ufs_sp(X.to_numpy(), 1, 2, 100, 100)
+    w = ufs_sp(X.to_numpy(), 1e3, 2, 100, 100)
