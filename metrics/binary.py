a29e3b5039edd518011e258e1f829226c07d91bd
yoavti <yoavti@post.bgu.ac.il>
2022-07-11 19:26:33 +0300

    Made binary_metric more readable

--- metrics/binary.py
+++ metrics/binary.py
@@ -3,11 +3,14 @@ import numpy as np
 
 def binary_metric(metric):
     def averaged(y_true, y_pred):
-        def label_comp(label):
+        def label_metric(label):
             y_true_bin = y_true == label
             y_pred_bin = y_pred == label
             if len(np.unique(y_true_bin)) != 2:
                 return 1
             return metric(y_true_bin, y_pred_bin)
-        return np.mean(np.vectorize(label_comp)(np.unique(np.concatenate((y_true, y_pred)))))
+        y_total = np.concatenate((y_true, y_pred))
+        labels = np.unique(y_total)
+        metric_values = np.vectorize(label_metric)(labels)
+        return np.mean(metric_values)
     return averaged
