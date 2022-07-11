7e7b887b2e041ae935775dc05c86b16c4e93577e
yoavti <yoavti@post.bgu.ac.il>
2022-07-11 12:52:35 +0300

    Added mrmr.py

---
+++ feature_selection/mrmr.py
@@ -0,0 +1,13 @@
+import numpy as np
+from sklearnex import patch_sklearn
+patch_sklearn()
+from skfeature.function.information_theoretical_based.MRMR import mrmr
+
+
+def mrmr_score(X, y):
+    F = mrmr(X, y)
+    values = np.arange(F.shape[0])[::-1]
+    ret = np.zeros(F.shape[0])
+    ret[F] = values
+    ret = ret.astype(int)
+    return ret
