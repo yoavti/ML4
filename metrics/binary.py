462880fcaf4ff685eed3a1a0fd7d58683c53b254
yoavti <yoavti@post.bgu.ac.il>
2022-07-11 19:23:44 +0300

    Added case when there is only one label in y_true

--- metrics/binary.py
+++ metrics/binary.py
@@ -3,11 +3,16 @@ import numpy as np
 
 def binary_metric(metric):
     def averaged(y_true, y_pred):
+        if len(np.unique(y_true)) != 2:
+            return 1
+
         def label_comp(label):
             y_true_bin = y_true == label
             y_pred_bin = y_pred == label
             if len(np.unique(y_true_bin)) != 2:
                 return 1
             return metric(y_true_bin, y_pred_bin)
+
         return np.mean(np.vectorize(label_comp)(np.unique(np.concatenate((y_true, y_pred)))))
+
     return averaged
