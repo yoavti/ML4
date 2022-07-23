3b670e41c93ca23bcf34c0d5f9bad19f13b5826b
yoavti <yoavti@post.bgu.ac.il>
2022-07-23 15:38:34 +0300

    Removed datasets where n >= d

--- data/experiments/scikit_feature_datasets/load.py
+++ data/experiments/scikit_feature_datasets/load.py
@@ -3,25 +3,21 @@ import os
 from data.utils import LabelFileLoader
 
 
-datasets = ['ALLAML', 'arcene', 'BASEHOCK', 'Carcinom', 'CLL-SUB-111', 'COIL20', 'colon', 'gisette', 'GLI-85', 'GLIOMA',
-            'Isolet', 'leukemia', 'lung', 'lung_small', 'lymphoma', 'madelon', 'nci9', 'ORL', 'orlraws10P', 'PCMAC',
-            'pixraw10P', 'Prostate-GE', 'RELATHE', 'SMK-CAN-187', 'TOX-171', 'USPS', 'warpAR10P', 'warpPIE10P', 'Yale']
+datasets = ['ALLAML', 'arcene', 'BASEHOCK', 'Carcinom', 'CLL-SUB-111', 'colon', 'GLI-85', 'GLIOMA', 'leukemia', 'lung',
+            'lung_small', 'lymphoma', 'nci9', 'ORL', 'orlraws10P', 'PCMAC', 'pixraw10P', 'Prostate-GE', 'RELATHE',
+            'SMK-CAN-187', 'TOX-171', 'warpAR10P', 'warpPIE10P', 'Yale']
 dataset_sizes = {'ALLAML': (7129, 72),
                  'arcene': (10000, 200),
                  'BASEHOCK': (4862, 1993),
                  'Carcinom': (9182, 174),
                  'CLL-SUB-111': (11340, 111),
-                 'COIL20': (1024, 1440),
                  'colon': (2000, 62),
-                 'gisette': (5000, 7000),
                  'GLI-85': (22283, 85),
                  'GLIOMA': (4434, 50),
-                 'Isolet': (617, 1560),
                  'leukemia': (7070, 72),
                  'lung': (3312, 203),
                  'lung_small': (325, 73),
                  'lymphoma': (4026, 96),
-                 'madelon': (500, 2600),
                  'nci9': (9712, 60),
                  'ORL': (1024, 400),
                  'orlraws10P': (10304, 100),
@@ -31,7 +27,6 @@ dataset_sizes = {'ALLAML': (7129, 72),
                  'RELATHE': (4322, 1427),
                  'SMK-CAN-187': (19993, 187),
                  'TOX-171': (5748, 171),
-                 'USPS': (256, 9298),
                  'warpAR10P': (2400, 130),
                  'warpPIE10P': (2420, 210),
                  'Yale': (1024, 165)}
