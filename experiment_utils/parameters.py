9a1c943892e1527a4ecc715156f9766e62da63ad
yoavti <yoavti@post.bgu.ac.il>
2022-07-23 18:24:05 +0300

    Added new ufs_sp_f algorithm to experiment code

--- experiment_utils/parameters.py
+++ experiment_utils/parameters.py
@@ -1,4 +1,4 @@
-from feature_selection import lfs, ufs_sp, mrmr_score, relief_f
+from feature_selection import lfs, ufs_sp_l_2_1, ufs_sp_f, mrmr_score, relief_f
 
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
@@ -7,7 +7,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 
 
-score_funcs = [lfs, ufs_sp, mrmr_score, relief_f]
+score_funcs = [lfs, ufs_sp_l_2_1, ufs_sp_f, mrmr_score, relief_f]
 classifiers = [KNeighborsClassifier(), GaussianNB(), LogisticRegression(), SVC(), RandomForestClassifier()]
 # ks = [1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 50, 100]
 ks = [10]
