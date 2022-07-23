d0f0ffb5f216e968aa40a1f3a3c875e50f6c6f07
yoavti <yoavti@post.bgu.ac.il>
2022-07-23 18:23:03 +0300

    Added lfs back

--- toy.py
+++ toy.py
@@ -6,7 +6,7 @@ from data import data_loader
 
 def main():
     X, y = data_loader.load('toy')
-    # alpha = lfs(X.to_numpy(), y.to_numpy())
+    alpha = lfs(X.to_numpy(), y.to_numpy())
     w = ufs_sp_l_2_1(X.to_numpy(), y.to_numpy())
 
 
