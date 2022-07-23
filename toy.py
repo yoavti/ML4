3682d299ce74d13c87e8bf790c38976ba35c5fa1
yoavti <yoavti@post.bgu.ac.il>
2022-07-23 18:22:13 +0300

    Changed to fit changes

--- toy.py
+++ toy.py
@@ -1,13 +1,13 @@
 from sklearnex import patch_sklearn
 patch_sklearn()
-from feature_selection import lfs, ufs_sp
+from feature_selection import lfs, ufs_sp_l_2_1
 from data import data_loader
 
 
 def main():
     X, y = data_loader.load('toy')
-    alpha = lfs(X.to_numpy(), y.to_numpy())
-    w = ufs_sp(X.to_numpy(), y.to_numpy())
+    # alpha = lfs(X.to_numpy(), y.to_numpy())
+    w = ufs_sp_l_2_1(X.to_numpy(), y.to_numpy())
 
 
 if __name__ == '__main__':
