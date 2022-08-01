c38a1e8ebb9597b202c96e86fcb0da0c09102a64
yoavti <yoavti@post.bgu.ac.il>
2022-08-01 11:08:39 +0300

    Only add SelectKBest to preprocess if necessary

--- experiment_utils/preprocess.py
+++ experiment_utils/preprocess.py
@@ -5,10 +5,12 @@ from sklearn.preprocessing import LabelEncoder
 
 
 def preprocess_steps(n):
-    return [('imputer', SimpleImputer()),
-            ('var_thresh', VarianceThreshold()),
-            ('transform', PowerTransformer()),
-            ('ff', SelectKBest(k='all' if n < 1000 else 1000))]
+    ret = [('imputer', SimpleImputer()),
+           ('var_thresh', VarianceThreshold()),
+           ('transform', PowerTransformer())]
+    if n >= 1000:
+        ret.append(('ff', SelectKBest(k=n)))
+    return ret
 
 
 def preprocess(X, y):
