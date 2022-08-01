8e2ae2dd229d5adbaccfc922bceddf1534b156b1
yoavti <yoavti@post.bgu.ac.il>
2022-08-01 15:26:59 +0300

    Added named_score_funcs and named_classifiers

--- experiment_utils/parameters.py
+++ experiment_utils/parameters.py
@@ -8,5 +8,16 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 score_funcs = [lfs, ufs_sp_l_2_1, ufs_sp_f, mrmr_score, relief_f]
+named_score_funcs = {'lfs': lfs,
+                     'ufs_sp_l_2_1': ufs_sp_l_2_1,
+                     'ufs_sp_f': ufs_sp_f,
+                     'mrmr_score': mrmr_score,
+                     'relief_f': relief_f}
+
 classifiers = [KNeighborsClassifier(), GaussianNB(), LogisticRegression(), SVC(), RandomForestClassifier()]
+named_classifiers = {'knn': KNeighborsClassifier(),
+                     'nb': GaussianNB(),
+                     'lr': LogisticRegression(),
+                     'svc': SVC(),
+                     'rf': RandomForestClassifier()}
 ks = [1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 50, 100]
