902723b3bbfb2247951a6276cc20226c0a11d897
yoavti <yoavti@post.bgu.ac.il>
2022-08-01 15:27:11 +0300

    Fixed aug_experiments.py

--- aug_experiments.py
+++ aug_experiments.py
@@ -13,7 +13,7 @@ from pprint import PrettyPrinter
 from data import data_loader
 from experiment_utils.cv import cv_method
 from experiment_utils.metrics import get_metrics
-from experiment_utils.parameters import score_funcs, classifiers, ks
+from experiment_utils.parameters import named_score_funcs, named_classifiers, ks
 from experiment_utils.preprocess import preprocess
 
 from sklearn.decomposition import KernelPCA
@@ -24,22 +24,16 @@ from imblearn.over_sampling import SMOTE
 
 pp = PrettyPrinter()
 
-fss = {score_func.__name__: SelectKBest(score_func) for score_func in score_funcs}
+fss = {name: SelectKBest(score_func) for name, score_func in named_score_funcs.items()}
 fss['select_fdr'] = SelectFdr(alpha=0.1)
-
-named_classifiers = {classifier.__name__: classifier for classifier in classifiers}
 
 parser = ArgumentParser(description='Data augmentation experiments.')
 parser.add_argument('-d', '--dataset', type=str, help='dataset')
 parser.add_argument('-fs', '--feature_selection', type=str, choices=list(fss), help='feature selection method')
 parser.add_argument('-clf', '--classifier', type=str, choices=list(named_classifiers), help='classifier')
-parser.add_argument('-k', '--n_features_to_select', type=int, choices=ks, help='number of features to select')
+parser.add_argument('-k', '--n_features_to_select', default=10, type=int, choices=ks,
+                    help='number of features to select')
 args = parser.parse_args()
-
-
-def insert_pca_columns(df, data, kernel):
-    n, d = data.shape
-    df[[f'{kernel}_{i}' for i in range(d)]] = data
 
 
 def run_aug(ds, fs, clf):
@@ -63,16 +57,15 @@ def run_aug(ds, fs, clf):
         clf = clone(clf_orig)
 
         fs.fit(X_train, y_train)
-        X_train = fs.transform(X_train, y_train)
-        X_test = fs.transform(X_test, y_test)
+        X_train = fs.transform(X_train)
+        X_test = fs.transform(X_test)
 
         kernels = ['linear', 'rbf']
-        pcas = {kernel: KernelPCA(kernel=kernel) for kernel in kernels}
-        reduced_X_trains = {kernel: pca.fit_transform(X_train) for kernel, pca in pcas.items()}
-        reduced_X_tests = {kernel: pca.transform(X_test) for kernel, pca in pcas.items()}
-        for kernel in kernels:
-            insert_pca_columns(X_train, reduced_X_trains[kernel], kernel)
-            insert_pca_columns(X_test, reduced_X_tests[kernel], kernel)
+        pcas = [KernelPCA(kernel=kernel) for kernel in kernels]
+        reduced_X_trains = [pca.fit_transform(X_train) for pca in pcas]
+        reduced_X_tests = [pca.transform(X_test) for pca in pcas]
+        X_train = np.hstack([X_train] + [X for X in reduced_X_trains])
+        X_test = np.hstack([X_test] + [X for X in reduced_X_tests])
 
         sm = SMOTE()
         X_train, y_train = sm.fit_resample(X_train, y_train)
@@ -88,7 +81,7 @@ def run_aug(ds, fs, clf):
 
 
 def main():
-    metric_values = run_aug(args.dataset, fss[args.feature_selector], named_classifiers[args.classifier])
+    metric_values = run_aug(args.dataset, fss[args.feature_selection], named_classifiers[args.classifier])
     pp.pprint(metric_values)
     with open(os.path.join('results', f'{args.dataset}_aug.json'), 'w+') as f:
         json.dump(metric_values, f)
