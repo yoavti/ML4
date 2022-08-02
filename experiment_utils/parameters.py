b7dd9f6f0bb519ec935cd35f9771240e99513810
yoavti <yoavti@post.bgu.ac.il>
2022-08-02 13:04:12 +0300

    Removed repeated definition of score funcs and classifiers

--- experiment_utils/parameters.py
+++ experiment_utils/parameters.py
@@ -7,17 +7,18 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 
 
-score_funcs = [lfs, ufs_sp_l_2_1, ufs_sp_f, mrmr_score, relief_f]
 named_score_funcs = {'lfs': lfs,
                      'ufs_sp_l_2_1': ufs_sp_l_2_1,
                      'ufs_sp_f': ufs_sp_f,
                      'mrmr_score': mrmr_score,
                      'relief_f': relief_f}
+score_funcs = list(named_score_funcs.values())
 
-classifiers = [KNeighborsClassifier(), GaussianNB(), LogisticRegression(), SVC(), RandomForestClassifier()]
 named_classifiers = {'knn': KNeighborsClassifier(),
                      'nb': GaussianNB(),
                      'lr': LogisticRegression(),
                      'svc': SVC(),
                      'rf': RandomForestClassifier()}
+classifiers = list(named_classifiers.values())
+
 ks = [1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 50, 100]
