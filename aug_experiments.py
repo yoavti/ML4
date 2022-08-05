e179dc726e07d17a2da2ae6bf562031e8fd73226
yoavti <yoavti@post.bgu.ac.il>
2022-08-05 12:16:05 +0300

    Made sure path exists

--- aug_experiments.py
+++ aug_experiments.py
@@ -97,10 +97,20 @@ def run_aug(ds, fs, clf):
     return metric_values
 
 
+def create_if_not_exists(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+
 def main():
     metric_values = run_aug(args.dataset, fss[args.feature_selection], named_classifiers[args.classifier])
     pp.pprint(metric_values)
-    results_path = os.path.join('results', 'aug', args.dataset)
+    results_path = 'results'
+    create_if_not_exists(results_path)
+    results_path = os.path.join(results_path, 'aug')
+    create_if_not_exists(results_path)
+    results_path = os.path.join(results_path, args.dataset)
+    create_if_not_exists(results_path)
     with open(os.path.join(results_path, 'res.json'), 'w+') as f:
         json.dump(metric_values, f)
 
