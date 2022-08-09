0f69c3a98da96fb9b6634503f51c817b5faa5614
yoavti <yoavti@post.bgu.ac.il>
2022-08-09 19:49:15 +0300

    Added run_aug_experiments.py

---
+++ run_aug_experiments.py
@@ -0,0 +1,13 @@
+import os
+
+import pandas as pd
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('best.csv')
+    for _, row in df.iterrows():
+        col_args_pairs = [('dataset', 'd'), ('fs', 'fs'), ('clf', 'clf'), ('k', 'k')]
+        command = 'aug_experiments.py'
+        for col, arg in col_args_pairs:
+            command += f' -{arg} {row[col]}'
+        os.system(command)
