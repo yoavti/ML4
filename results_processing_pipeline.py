c08df658be99620ef87c90957a5fed5c63f2fddf
yoavti <yoavti@post.bgu.ac.il>
2022-08-15 00:31:25 +0300

    Removed parent from is_dataset_available call

---
+++ results_processing_pipeline.py
@@ -0,0 +1,16 @@
+from clean import clean
+from missing import find_missing
+from results_processing import find_best, aggregate_results
+from run_aug_experiments import run_aug_experiments
+from calc_stat import statistical_tests
+from compare import compare
+
+
+if __name__ == '__main__':
+    clean()
+    find_missing()
+    find_best()
+    run_aug_experiments()
+    statistical_tests()
+    compare()
+    aggregate_results()
