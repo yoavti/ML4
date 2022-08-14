1acbe3b963fa1dd076481e4efa1bae0a7d2ef29e
yoavti <yoavti@post.bgu.ac.il>
2022-08-15 01:39:11 +0300

    Removed compare

--- results_processing_pipeline.py
+++ results_processing_pipeline.py
@@ -3,7 +3,7 @@ from missing import find_missing
 from results_processing import find_best, aggregate_results
 from run_aug_experiments import run_aug_experiments
 from calc_stat import statistical_tests
-from compare import compare
+from compare import aug, improvement
 
 
 if __name__ == '__main__':
@@ -12,5 +12,6 @@ if __name__ == '__main__':
     find_best()
     run_aug_experiments()
     statistical_tests()
-    compare()
+    aug()
+    improvement()
     aggregate_results()
