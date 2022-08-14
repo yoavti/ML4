024519a526c515088d39f8229e343f6c4abc42d9
yoavti <yoavti@post.bgu.ac.il>
2022-08-14 20:13:08 +0300

    Now also specifies folders with missing files

--- missing.py
+++ missing.py
@@ -6,14 +6,14 @@ from experiment_utils.parameters import ks
 from experiment_utils.parameters import named_score_funcs
 
 
-def add_dict_row(d, dataset, k=None, file=None):
+def add_dict_row(d, dataset, k=None, files=None):
     d['dataset'].append(dataset)
     d['k'].append(k)
-    d['file'].append(file)
+    d['files'].append(files)
 
 
 def find_missing():
-    missing_dict = dict(dataset=[], k=[], file=[])
+    missing_dict = dict(dataset=[], k=[], files=[])
     fss = list(named_score_funcs) + ['SelectFdr']
     results_path = 'results'
     with open('datasets.txt', 'r') as f:
@@ -29,13 +29,12 @@ def find_missing():
             if not os.path.exists(k_results_path):
                 add_dict_row(missing_dict, dataset, k)
                 continue
-            # files = ['cv_results.csv', 'fs.csv', 'best.json']
-            # fs_files = [f'{fs}.csv' for fs in fss]
-            # files += fs_files
-            # for file in files:
-            #     file_path = os.path.join(k_results_path, file)
-            #     if not os.path.exists(file_path):
-            #         add_dict_row(missing_dict, dataset, k, file)
+            files = ['cv_results.csv', 'fs.csv', 'best.json']
+            fs_files = [f'{fs}.csv' for fs in fss]
+            files += fs_files
+            missing_files = [file for file in files if not os.path.exists(os.path.join(k_results_path, file))]
+            if missing_files:
+                add_dict_row(missing_dict, dataset, k, missing_files)
     missing_df = pd.DataFrame(missing_dict)
     missing_df.to_csv('missing.csv', index=False)
 
