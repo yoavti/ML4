6cd5155c1587c621510857cab58bd557c02d55ac
yoavti <yoavti@post.bgu.ac.il>
2022-08-14 21:59:51 +0300

    Added improvement

--- compare.py
+++ compare.py
@@ -7,6 +7,7 @@ from functools import partial
 
 from data.experiments import ARFF, bioconductor, Datamicroarray, scikit_feature_datasets
 from experiment_utils.metrics import get_metrics
+from experiment_utils.parameters import named_classifiers, ks
 
 
 def func_to_name(func):
@@ -35,7 +36,7 @@ def read_cv_results(path):
 
 
 def aug():
-    results_dict = {}
+    res_dict = {}
     results_path = 'results'
     metric_names = list(get_metrics())
     columns_mapping = {f'mean_test_{metric}': metric for metric in metric_names}
@@ -79,15 +80,63 @@ def aug():
             cv_results = cv_results.mean()
             comparison_dict = dict(aug=aug_results, original=cv_results)
             comparison_df = pd.DataFrame(comparison_dict)
-            results_dict[dataset] = comparison_df
-    return results_dict
+            res_dict[dataset] = comparison_df
+    return res_dict
+
+
+def improvement():
+    res_dict = {}
+    results_path = 'results'
+    metric_names = list(get_metrics())
+    columns_mapping = {f'mean_test_{metric}': metric for metric in metric_names}
+    columns_mapping['mean_fit_time'] = 'time'
+    param_columns = ['param_fs__transformer', 'param_fs__transformer__score_func', 'param_clf__estimator']
+    directories = [ARFF, bioconductor, Datamicroarray, scikit_feature_datasets]
+    for directory in directories:
+        for dataset in directory.load.datasets:
+            dataset_results_path = os.path.join(results_path, dataset)
+            if not os.path.exists(dataset_results_path):
+                continue
+            for k in ks:
+                k_results_path = os.path.join(dataset_results_path, str(k))
+                if not os.path.exists(k_results_path):
+                    continue
+                cv_results_path = os.path.join(k_results_path, 'cv_results.csv')
+                if not os.path.exists(cv_results_path):
+                    continue
+                fs_path = os.path.join(k_results_path, 'fs.csv')
+                if not os.path.exists(fs_path):
+                    continue
+                cv_results = read_cv_results(cv_results_path)
+                cv_results = cv_results[list(columns_mapping) + param_columns]
+                cv_results = cv_results[cv_results['param_fs__transformer'] == 'SelectKBest']
+                cv_results = cv_results.drop('param_fs__transformer', axis=1)
+                comparison_dict = {}
+                for score_func in ['ufs_sp_l_2_1', 'ufs_sp_f']:
+                    score_cv_results = cv_results[cv_results['param_fs__transformer__score_func'] == score_func]
+                    score_cv_results = score_cv_results.drop('param_fs__transformer__score_func', axis=1)
+                    for classifier in named_classifiers:
+                        meta_clf = f'{score_func}->{classifier}'
+                        classifier_cv_results = score_cv_results[score_cv_results['param_clf__estimator'] == classifier]
+                        classifier_cv_results = classifier_cv_results.drop('param_clf__estimator', axis=1)
+                        classifier_cv_results = classifier_cv_results.rename(columns_mapping, axis=1)
+                        classifier_cv_results = classifier_cv_results.mean()
+                        comparison_dict[meta_clf] = classifier_cv_results
+                comparison_df = pd.DataFrame(comparison_dict)
+                res_dict[dataset] = comparison_df
+    return res_dict
+
+
+def present_results(func):
+    print(func.__name__)
+    for dataset, df in func().items():
+        print(dataset)
+        print(df)
 
 
 def main():
-    print('aug')
-    for dataset, df in aug().items():
-        print(dataset)
-        print(df)
+    present_results(aug)
+    present_results(improvement)
 
 
 if __name__ == '__main__':
