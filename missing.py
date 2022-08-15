0b6c8f6d453d01ae8b8c3717d25ddbfcb9b97e10
yoavti <yoavti@post.bgu.ac.il>
2022-08-15 09:04:38 +0300

    Removed files

--- missing.py
+++ missing.py
@@ -6,14 +6,13 @@ from experiment_utils.parameters import ks
 from experiment_utils.parameters import named_score_funcs
 
 
-def add_dict_row(d, dataset, k=None, files=None):
+def add_dict_row(d, dataset, k=None):
     d['dataset'].append(dataset)
     d['k'].append(k)
-    d['files'].append(files)
 
 
 def find_missing():
-    missing_dict = dict(dataset=[], k=[], files=[])
+    missing_dict = dict(dataset=[], k=[])
     fss = list(named_score_funcs) + ['SelectFdr']
     results_path = 'results'
     with open('datasets.txt', 'r') as f:
@@ -32,9 +31,10 @@ def find_missing():
             files = ['cv_results.csv', 'fs.csv', 'best.json']
             fs_files = [f'{fs}.csv' for fs in fss]
             files += fs_files
-            missing_files = [file for file in files if not os.path.exists(os.path.join(k_results_path, file))]
-            if missing_files:
-                add_dict_row(missing_dict, dataset, k, missing_files)
+            for file in files:
+                path = os.path.join(k_results_path, file)
+                if not os.path.exists(path):
+                    add_dict_row(missing_dict, dataset, k)
     missing_df = pd.DataFrame(missing_dict)
     missing_df.to_csv('missing.csv', index=False)
 
