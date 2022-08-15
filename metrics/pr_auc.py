65ab95cdb3a6eee034dd40183e8f8059a8f79f66
yoavti <yoavti@post.bgu.ac.il>
2022-08-15 16:50:28 +0300

    pr_auc_score now catches value errors and returns 0

--- metrics/pr_auc.py
+++ metrics/pr_auc.py
@@ -4,4 +4,8 @@ from sklearn.metrics import precision_recall_curve, auc
 def pr_auc_score(y_true, probas_pred, *, pos_label=None, sample_weight=None):
     precision, recall, threshold = precision_recall_curve(y_true, probas_pred,
                                                           pos_label=pos_label, sample_weight=sample_weight)
-    return auc(precision, recall)
+    try:
+        return auc(precision, recall)
+    except ValueError as e:
+        print(e)
+        return 0
